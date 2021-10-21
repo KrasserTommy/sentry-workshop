efe09bdb2653ab157fdb0ef86906cb2495ecce2a
Thomas Schlömer <schloemer.thomas@gmail.com>
2021-10-21 13:49:37 +0200

    fix file structure

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { CustomErrorBoundary } from "./CustomErrorBoundary";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+  integrations: [new Integrations.BrowserTracing()],
+  release: "0.1.0",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <CustomErrorBoundary>
+      <App />
+    </CustomErrorBoundary>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
