ae6f99bb6f019470dad8653bab4b93d22f0d652b
Thomas Schlömer <schloemer.thomas@gmail.com>
2021-10-29 12:26:58 +0200

    add tracing

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Integrations } from "@sentry/tracing";
 
 // Sentry.init({
 //   dsn: process.env.REACT_APP_SENTRY_DSN,
+// integrations: [new Integrations.BrowserTracing()],
 //   release: "0.1.0",
 //   debug: true,
 // });
