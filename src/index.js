1142156b976a7ba70752b6e3f6265dd09d37e161
Thomas Schlömer <schloemer.thomas@gmail.com>
2021-10-29 12:51:24 +0200

    simplify

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
 
 // Sentry.init({
 //   dsn: process.env.REACT_APP_SENTRY_DSN,
-// integrations: [new Integrations.BrowserTracing()],
 //   release: "0.1.0",
 //   debug: true,
 // });
